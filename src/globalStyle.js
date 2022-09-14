56e063812c848d464375a5471a81fe2a9bf07129
unknown <ononobip@gmail.com>
2022-09-14 23:21:12 +0100

    First Commit

---
+++ src/globalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-size: 62.5%;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+body{
+    background-color: white;
+
+}
+`
