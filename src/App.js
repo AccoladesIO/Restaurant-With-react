051a08192528020b38c4962408017941e5a14132
unknown <ononobip@gmail.com>
2022-09-14 23:25:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router} from "react-router-dom";
+import Hero from "./components/Hero/hero";
+import Main from "./components/Main/main";
+import { GlobalStyle } from "./globalStyle";
+// import { Link, Routes, Route } from "react-router-dom";
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle></GlobalStyle>
+      <Hero></Hero>
+      <Main></Main>
+    </Router>
   );
-}
+};
 
 export default App;
