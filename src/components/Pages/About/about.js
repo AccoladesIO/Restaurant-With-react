a2a0f07d84d9c87df54afceec7d0f54ee3e777cd
unknown <ononobip@gmail.com>
2022-09-15 23:23:33 +0100

    first commit

---
+++ src/components/Pages/About/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'Right',
+          alignItems: 'Right',
+          height: '100vh'
+        }}
+      >
+        <h1>GeeksforGeeks is a Computer Science portal for geeks.</h1>
+      </div>
+    );
+  };
+    
+  export default About;
+  
---
+++ src/components/Pages/Home/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../../Hero/hero'
+import Main from '../../Main/main'
+
+
+const Home = () => {
+  return (
+    <div>
+    <Hero></Hero>
+      <Main></Main>
+    </div>
+  )
+}
+
+export default Home;
