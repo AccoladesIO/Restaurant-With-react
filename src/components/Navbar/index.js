04119f7c98a92dfd55f3e69ee83cd72c2f8f8075
unknown <ononobip@gmail.com>
2022-09-15 23:28:30 +0100

    updated commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,41 @@
 import React from "react";
-import { Nav, LogoBox} from "./NavbarElements";
-import LogoImg from '../../image/logo.svg'
-import Lists from "../List/List";
+import { LogoBox } from "./NavbarElements";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+import LogoImg from "../../image/logo.svg";
 
 const Navbar = () => {
   return (
     <>
       <Nav>
-        <LogoBox><img src={LogoImg} alt="FoodBoard Logo" /></LogoBox>
-        <Lists> {Lists.map((ListItem) =>{
-          const {id, name, path} = ListItem;
-          return(
-          <ul>
-            <li><a href={path} key={id}> {name} </a></li>
-          </ul>
-          )
-        })}</Lists>
+        <LogoBox>
+          <img src={LogoImg} alt="FoodBoard Logo" />
+        </LogoBox>
+        <Bars />
+
+        <NavMenu>
+          <NavLink to="/" activeStyle>
+            Home
+          </NavLink>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/pricing" activeStyle>
+            Pricing
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            Contact
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/signin">Sign In</NavBtnLink>
+        </NavBtn>
       </Nav>
     </>
   );
