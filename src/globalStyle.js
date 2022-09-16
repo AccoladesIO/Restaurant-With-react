a86e3846d1560658d3685762b59a34d366ac52ef
unknown <ononobip@gmail.com>
2022-09-16 16:18:58 +0100

    new commit

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -12,4 +12,38 @@ body{
     background-color: white;
 
 }
+.section-featured{
+    padding: 4.6rem;
+    max-width: 90%;
+    margin: auto;
+}
+.heading-featured-in{
+    font-size: 1.4rem;
+    text-transform: uppercase;
+    letter-spacing: 0.75px;
+    font-weight: 600;
+    text-align: center;
+    margin-bottom: 2.4rem;
+    color: #888;
+}
+.logos{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    row-gap: 2rem;
+}
+.logos img{
+    height: 2.2rem;
+    filter: brightness(0);
+    opacity: 50%;
+    &:hover{
+        filter: brightness(1);
+        opacity: 100%;
+        transition: 0.4s ease-in-out;
+    }
+    @media screen and (max-width: 428px){
+        height: 2rem;
+    }
+}
 `
