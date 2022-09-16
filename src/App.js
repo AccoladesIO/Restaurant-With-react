f2b2b4bb5c20de749f24b9d84e109300071a4205
unknown <ononobip@gmail.com>
2022-09-16 16:26:20 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router} from "react-router-dom";
-import Hero from "./components/Hero/hero";
-import Main from "./components/Main/main";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { GlobalStyle } from "./globalStyle";
+import Navbar from "./components/Navbar";
+import Home from "./components/Pages/Home/home";
+import About from "./components/Pages/About/about";
+import Pricing from "./components/Pages/Pricing/pricing";
 // import { Link, Routes, Route } from "react-router-dom";
 
 
@@ -10,9 +12,13 @@ import { GlobalStyle } from "./globalStyle";
 const App = () => {
   return (
     <Router>
-      <GlobalStyle></GlobalStyle>
-      <Hero></Hero>
-      <Main></Main>
+      <GlobalStyle/>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/pricing' element={<Pricing/>} />
+      </Routes>  
     </Router>
   );
 };
